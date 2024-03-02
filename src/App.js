ca92a708cea75550389e3fdb33d7cf8892af24ae
du than <du.than@relexsolutions.com>
2024-03-02 13:57:40 +0200

    Init

--- src/App.js
+++ src/App.js
@@ -1,25 +1,90 @@
-import logo from './logo.svg';
-import './App.css';
+import { useState } from 'react'
+import "@chatscope/chat-ui-kit-styles/dist/default/styles.min.css";
+import {
+  MainContainer,
+  ChatContainer,
+  MessageList,
+  Message,
+  MessageInput,
+  TypingIndicator
+} from "@chatscope/chat-ui-kit-react";
+
+const API_KEY = "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";
 
 function App() {
+  const [typing, setTyping] = useState(false);
+  const [messages, setMessages] = useState([
+    {
+      message: "Hello Eena, I am BigBugBot, I am here to replace Dudu, he is sleeping right now!",
+      sender: "DuDu"
+    }
+  ]);
+
+  async function onSend(message) {
+    const newMessage = {
+      message: message,
+      sender: "user",
+      direction: "outgoing"
+    }
+    const newMessages = [...messages, newMessage];
+    setMessages(newMessages);
+    setTyping(true);
+    await processMessageToChatGPT(newMessages);
+  };
+
+  async function processMessageToChatGPT(chatMessages) {
+    const apiMessages = chatMessages.map(messageObj => {
+      return {
+        role: messageObj.sender === "DuDu" ? "assistant" : "user",
+        content: messageObj.message
+      }
+    });
+
+    const systemMessage = {
+      role: "system",
+      content: "You will play me when I am sleeping and your job is answering to my girl friend Eena. She will chat with you."
+    };
+
+    const payload = {
+      "model": "gpt-3.5-turbo",
+      "messages": [systemMessage, ...apiMessages]
+    };
+
+    await fetch("https://api.openai.com/v1/chat/completions", {
+      method: "POST",
+      headers: {
+        "Authorization": "Bearer " + API_KEY,
+        "Content-Type": "application/json"
+      },
+      body: JSON.stringify(payload)
+    }).then(data => {
+      return data.json();
+    }).then(data => {
+      setMessages([...chatMessages, {
+        message: data.choices[0].message.content,
+        sender: "DuDu"
+      }]);
+      setTyping(false);
+    });
+  }
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
+    <MainContainer>
+      <ChatContainer>
+        <MessageList
+          scrollBehavior='smooth'
+          typingIndicator={typing ? <TypingIndicator content="DuDu is typing"/> : null}
         >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+          {
+            messages.map((message, index) => {
+              return <Message key={index} model={message} />
+            })
+          }
+        </MessageList>
+        <MessageInput placeholder="Type message here" onSend={onSend}/>
+      </ChatContainer>
+    </MainContainer>
+  )
 }
 
-export default App;
+export default App
